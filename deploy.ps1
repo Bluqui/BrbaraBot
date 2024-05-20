95997518aff1a8880f48942148e26251a1bc57df
Bluqui <lucasultimat10@gmail.com>
2024-05-20 12:30:47 -0300

    0.1.0

--- deploy.ps1
+++ deploy.ps1
@@ -2,6 +2,6 @@ Write-Host "Adicionando './'" -ForegroundColor Cyan
 git add ./
 Write-Host "`nFazendo Commit" -ForegroundColor Blue
 git commit -m "0.1.0"
-Write-Host "`nDado o Push" -ForegroundColor DarkBlue
+Write-Host "`nDando o Push" -ForegroundColor DarkBlue
 git push
 Write-Host "`n"
