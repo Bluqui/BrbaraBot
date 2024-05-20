d2c1362132063f854611541e190c203a302387c5
Bluqui <lucasultimat10@gmail.com>
2024-05-20 12:41:06 -0300

    0.1.0

--- deploy.ps1
+++ deploy.ps1
@@ -5,3 +5,19 @@ git commit -m "0.1.0"
 Write-Host "`nDando o Push" -ForegroundColor DarkBlue
 git push
 Write-Host "`n"
+
+$confirmSlashDeploy = Read-Host "Registrar os ShashCommands? [y] - [n]"
+if ($confirmSlashDeploy -eq 'Y') {
+    Write-Host "Registrando ShashCommands..."
+    node .\main\deploy-commands.js
+} elseif ($confirmSlashDeploy -eq 'N'){
+    Write-Host "Registro cancelado"
+} else {return}
+
+$confirmSlashDeploy = Read-Host "Iniciar? [y] - [n]"
+if ($confirmIni -eq 'Y') {
+    Write-Host "Iniciando..."
+    node .
+} elseif ($confirmIni -eq 'N'){
+    Write-Host "Inicialização cancelada"
+} else {return}
