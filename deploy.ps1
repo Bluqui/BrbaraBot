21cce73186df683ca23b44993fb2d01b7243ed5b
Bluqui <lucasultimat10@gmail.com>
2024-05-23 10:29:05 -0300

    0.2.0

--- deploy.ps1
+++ deploy.ps1
@@ -1,28 +1,57 @@
-Write-Host "Registrar os ShashCommands?" -ForegroundColor Cyan
-do {
-    $confirmSlashDeploy = Read-Host "[y] - [n]"
-    if ($confirmSlashDeploy -eq 'Y') {
-        Write-Host "Registrando ShashCommands..."
-        node .\main\deploy-commands.js
-    } elseif ($confirmSlashDeploy -eq 'N'){
-        Write-Host "Registro cancelado"
-    } else {}
-} while ($confirmSlashDeploy -ne 'Y' -and $confirmSlashDeploy -ne 'N')
+param (
+    [switch]$y
+)
 
-Write-Host "Adicionando todos os arquivos" -ForegroundColor Cyan
-git add ./
-Write-Host "`nFazendo Commit" -ForegroundColor Blue
-git commit -m "0.2.0"
-Write-Host "`nDando o Push" -ForegroundColor DarkBlue
-git push origin HEAD:two
+if(!$y) {
 
-Write-Host "Iniciar?" -ForegroundColor Cyan
-do {
-    $confirmIni = Read-Host "[y] - [n]"
-    if ($confirmIni -eq 'Y') {
-        Write-Host "Iniciando..."
-        nodemon /main/index.js
-    } elseif ($confirmIni -eq 'N'){
-        Write-Host "Inicialização cacelada"
-    } else {}
-} while ($confirmIni -ne 'Y' -and $confirmIni -ne 'N')
+
+    Write-Host "Registrar os ShashCommands?" -ForegroundColor Cyan
+    do {
+        $confirmSlashDeploy = Read-Host "[y] - [n]"
+        if ($confirmSlashDeploy -eq 'Y') {
+            Write-Host "Registrando ShashCommands..."
+            node .\main\deploy-commands.js
+        } elseif ($confirmSlashDeploy -eq 'N'){
+            Write-Host "Registro cancelado"
+        } else {}
+    } while ($confirmSlashDeploy -ne 'Y' -and $confirmSlashDeploy -ne 'N')
+
+
+    Write-Host "Adicionando todos os arquivos" -ForegroundColor Cyan
+    git add ./
+    Write-Host "`nFazendo Commit" -ForegroundColor Blue
+    git commit -m "0.2.0"
+    Write-Host "`nDando o Push" -ForegroundColor DarkBlue
+    git push origin HEAD:two
+
+
+    Write-Host "Iniciar?" -ForegroundColor Cyan
+    do {
+        $confirmIni = Read-Host "[y] - [n]"
+        if ($confirmIni -eq 'Y') {
+            Write-Host "Iniciando..."
+            nodemon /main/index.js
+        } elseif ($confirmIni -eq 'N'){
+            Write-Host "Inicialização cacelada"
+        } else {}
+    } while ($confirmIni -ne 'Y' -and $confirmIni -ne 'N')
+
+
+} else {
+
+
+    Write-Host "Registrando ShashCommands..."
+    node .\main\deploy-commands.js
+
+    Write-Host "Adicionando todos os arquivos" -ForegroundColor Cyan
+    git add ./
+    Write-Host "`nFazendo Commit" -ForegroundColor Blue
+    git commit -m "0.2.0"
+    Write-Host "`nDando o Push" -ForegroundColor DarkBlue
+    git push origin HEAD:two
+
+    Write-Host "Iniciando..."
+    nodemon /main/index.js
+
+    
+}
